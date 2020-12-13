80e8d4eea065679f1855962d4200623fd1c9c68e
xcoders <57432765+xcoders-hub@users.noreply.github.com>
2020-12-13 12:26:00 +0530

    Update config.py

--- tgfilestream/config.py
+++ tgfilestream/config.py
@@ -19,7 +19,7 @@ import os
 from yarl import URL
 
 try:
-    port = int(os.environ.get("PORT", "8080"))
+    port = 8080
 except ValueError:
     port = -1
 if not 1 <= port <= 65535:
@@ -27,16 +27,16 @@ if not 1 <= port <= 65535:
     sys.exit(1)
 
 try:
-    api_id = int(os.environ["TG_API_ID"])
-    api_hash = os.environ["TG_API_HASH"]
+    api_id = 1064864
+    api_hash = "5f3eeab0e6108731551e6a93598b654c"
 except (KeyError, ValueError):
     print("Please set the TG_API_ID and TG_API_HASH environment variables correctly")
     print("You can get your own API keys at https://my.telegram.org/apps")
     sys.exit(1)
 
-trust_headers = bool(os.environ.get("TRUST_FORWARD_HEADERS"))
-host = os.environ.get("HOST", "localhost")
-public_url = URL(os.environ.get("PUBLIC_URL", f"http://{host}:{port}"))
+trust_headers = 'false'
+host = "localhost"
+public_url = "http://cf32.herokuapp.com"
 
 session_name = os.environ.get("TG_SESSION_NAME", "tgfilestream")
 
@@ -45,20 +45,20 @@ debug = bool(os.environ.get("DEBUG"))
 
 try:
     # The per-user ongoing request limit
-    request_limit = int(os.environ.get("REQUEST_LIMIT", "5"))
+    request_limit = 5
 except ValueError:
     print("Please make sure the REQUEST_LIMIT environment variable is an integer")
     sys.exit(1)
 
 try:
     # The per-DC connection limit
-    connection_limit = int(os.environ.get("CONNECTION_LIMIT", "20"))
+    connection_limit =20
 except ValueError:
     print("Please make sure the CONNECTION_LIMIT environment variable is an integer")
     sys.exit(1)
 
 
-start_message = os.environ.get("TG_START_MESG", "Send an image or file to get a link to download it")
+start_message = "Send an image or file to get a link to download it"
 group_chat_message = os.environ.get("TG_G_C_MESG", "Sorry. But, I only work in private.")
 
-tg_bot_token = os.environ.get("TG_BOT_FATHER_TOKEN", None)
+tg_bot_token = "1225137582:AAGpFuy-wN75GSuZxs2N3DGCoyzBeoRsnbU"
